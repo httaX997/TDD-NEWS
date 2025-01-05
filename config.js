c96bd245cc422d97226c1020ce2a337b5de228c1
TDD-GANGS <Technicaldarkdevils@gmail.com>
2025-01-05 17:15:47 +0530

    TDD NEWS BOT BY TDD GANGS

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: "VAJIRA-MD=7c9BnLzL#TKA0leroxWNZ1yihpRFmAUv7LXqWKIpy-2LSnpvjwM0",
+    SESSION_ID: "",
     OWNER_NUMBER: '94711453361',
    // Put news sender jid below you need to send news
     hiru: '120363190669566051@g.us',
