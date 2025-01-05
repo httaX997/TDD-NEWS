6a726e037c6b7eeb075692e7dbb4552fc6740100
TDD-GANGS <Technicaldarkdevils@gmail.com>
2025-01-05 18:25:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: "",
+    SESSION_ID: 'Your session id here',
     OWNER_NUMBER: '94711453361',
    // Put news sender jid below you need to send news
     hiru: '120363190669566051@g.us',
