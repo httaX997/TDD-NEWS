783ae57e1f0c9599df9d9015cd3037bf16d0ee4d
TDD-GANGS <Technicaldarkdevils@gmail.com>
2025-01-05 22:39:10 +0530

    TDD NEWS BOT BY TDD GANGS

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=KV1yhTpJ#dL03I-uTioTUW26br3Qu8oAkJoyZzD6wS6Y8EjXB560' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
     OWNER_NUMBER: '94711453361',
    // Put news sender jid below you need to send news
-    hiru: '120363383902323952@g.us',
-    lankadeepa: '120363383902323952@g.us',
-    itn: '120363383902323952@g.us',
-    bbc: '120363383902323952@g.us',
-    gossiplankanews: '120363383902323952@g.us',
-    dasathalankanews: '120363383902323952@g.us',
-    siyatha: '120363383902323952@g.us',
-    derana: '120363383902323952@g.us'
+    hiru: '120363190669566051@g.us',
+    lankadeepa: '120363190669566051@g.us',
+    itn: '120363190669566051@g.us',
+    bbc: '120363190669566051@g.us',
+    gossiplankanews: '120363190669566051@g.us',
+    dasathalankanews: '120363190669566051@g.us',
+    siyatha: '120363190669566051@g.us',
+    derana: '120363190669566051@g.us'
 };
 
